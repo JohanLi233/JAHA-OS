f81aedd2b8a127f19a8ed77632534bae8e50eae5
JohanLi2333 <k20020910@kcl.ac.uk>
2022-02-17 16:32:52 +0000

    types.h

---
+++ src/include/JAHA_OS/type.h
@@ -0,0 +1,27 @@
+#ifndef JAHA_OS_TYPE_H
+#define JAHA_OS_TYPE_H
+
+#define ENF -1 //end of file
+
+#define NULL 0 //nuii
+
+#define bool _Bool
+#define true 1
+#define false 0
+
+#define _packed __attribute__((packed))
+
+typedef unsigned int size_t;
+typedef char int8;
+typedef short int16;
+typedef int int32;
+typedef long long int64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+
+
+#endif
