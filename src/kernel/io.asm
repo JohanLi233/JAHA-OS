5cd72054f25d3baae11c17b80bc934f29fc1e0ba
JohanLi2333 <k20020910@kcl.ac.uk>
2022-02-18 20:11:25 +0000

    io for build

--- src/kernel/io.asm
+++ src/kernel/io.asm
@@ -16,3 +16,49 @@ inb:
 
   leave
   ret
+
+  global outb
+outb:
+  push ebp
+  mov ebp, esp                  ;保存帧
+
+  mov edx, [ebp + 8]            ;port
+  mov eax, [ebp + 12]           ;value
+  out dx, al                    ;将al中的8bit输出到端口号dx
+
+  jmp $+2
+  jmp $+2
+
+  leave
+  ret
+
+  global inw
+inw:
+  push ebp
+  mov ebp, esp                  ;保存帧
+
+  xor eax, eax
+  mov edx, [ebp + 8]            ;port
+  in ax, dx                     ;将端口号dx的16bit输入到ax
+
+  jmp $+2
+  jmp $+2
+
+  leave
+  ret
+
+  global outw
+outw:
+  push ebp
+  mov ebp, esp                  ;保存帧
+
+  mov edx, [ebp + 8]            ;port
+  mov eax, [ebp + 12]           ;value
+  out dx, ax                    ;将al中的16bit输出到端口号dx
+
+  jmp $+2
+  jmp $+2
+
+  leave
+  ret
+
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,13 +2,17 @@
 #include <JAHA_OS/types.h>
 #include <JAHA_OS/io.h>
 
-#define CRT_ADDR_REG 0x3d5
-#define CRT_DATA_REG 0x3d6
+#define CRT_ADDR_REG 0x3d4
+#define CRT_DATA_REG 0x3d5
 #define CRT_CURSOR_H 0xe
 #define CRT_CURSOR_L 0xf
 
 void kernel_init()
 {
-  u8 data = inb(CRT_DATA_REG);
+  outb(CRT_ADDR_REG, CRT_CURSOR_H);
+  u16 pos = inb(CRT_DATA_REG) << 8;
+  outb(CRT_ADDR_REG, CRT_CURSOR_L);
+  pos |= inb(CRT_DATA_REG);
 
+  return
 }
