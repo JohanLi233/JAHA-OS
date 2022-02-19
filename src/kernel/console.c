b712a26a755faa2803932f33b1284b8fb27c3ce5
JohanLi2333 <k20020910@kcl.ac.uk>
2022-02-19 11:58:57 +0000

    console

--- src/kernel/console.c
+++ src/kernel/console.c
@@ -33,7 +33,7 @@ static u32 screen; // 当前显示器开始的内存位置
 
 static u32 pos; // 记录当前光标的内存位置
 
-static u32 x, y; // 当前光标的坐标
+static x, y; // 当前光标的坐标
 
 static u8 attr = 7;        // 字符样式
 static u16 erase = 0x0720; // 空格
@@ -41,176 +41,175 @@ static u16 erase = 0x0720; // 空格
 // 获得当前显示器的开始位置
 static void get_screen()
 {
-  outb(CRT_ADDR_REG, CRT_START_ADDR_H); // 开始位置高地址
-  screen = inb(CRT_DATA_REG) << 8; // 开始位置高八位
-  outb(CRT_ADDR_REG, CRT_START_ADDR_L);
-  screen |= inb(CRT_DATA_REG);
+    outb(CRT_ADDR_REG, CRT_START_ADDR_H); // 开始位置高地址
+    screen = inb(CRT_DATA_REG) << 8;      // 开始位置高八位
+    outb(CRT_ADDR_REG, CRT_START_ADDR_L);
+    screen |= inb(CRT_DATA_REG);
 
-  screen <<= 1; // screen *= 2
-  screen += MEM_BASE;
+    screen <<= 1; // screen *= 2
+    screen += MEM_BASE;
 }
 
-// 设置当前显示器的开始位置
+// 设置当前显示器开始的位置
 static void set_screen()
 {
-  outb(CRT_ADDR_REG, CRT_START_ADDR_H); // 开始位置高地址
-  outb(CRT_ADDR_REG, ((screen - MEM_BASE) >> 9) & 0xff);
-  outb(CRT_ADDR_REG, CRT_START_ADDR_L);
-  outb(CRT_ADDR_REG, ((screen - MEM_BASE) >> 1) & 0xff);
+    outb(CRT_ADDR_REG, CRT_START_ADDR_H); // 开始位置高地址
+    outb(CRT_DATA_REG, ((screen - MEM_BASE) >> 9) & 0xff);
+    outb(CRT_ADDR_REG, CRT_START_ADDR_L);
+    outb(CRT_DATA_REG, ((screen - MEM_BASE) >> 1) & 0xff);
 }
 
-// 获取当前光标位置
+// 获得当前光标位置
 static void get_cursor()
 {
-  outb(CRT_ADDR_REG, CRT_CURSOR_H); // 高地址
-  pos = inb(CRT_DATA_REG) << 8; // 高八位
-  outb(CRT_ADDR_REG, CRT_CURSOR_L);
-  pos |= inb(CRT_DATA_REG);
+    outb(CRT_ADDR_REG, CRT_CURSOR_H); // 高地址
+    pos = inb(CRT_DATA_REG) << 8;     // 高八位
+    outb(CRT_ADDR_REG, CRT_CURSOR_L);
+    pos |= inb(CRT_DATA_REG);
 
-  get_screen();
+    get_screen();
 
-  pos <<=1;
-  pos += MEM_BASE;
+    pos <<= 1; // pos *= 2
+    pos += MEM_BASE;
 
-  u32 delta = (pos - screen) >> 1;
-  x = delta % WIDTH;
-  y = delta / WIDTH;
+    u32 delta = (pos - screen) >> 1;
+    x = delta % WIDTH;
+    y = delta / WIDTH;
 }
 
 static void set_cursor()
 {
-  outb(CRT_ADDR_REG, CRT_CURSOR_H); // 光标高地址
-  outb(CRT_DATA_REG, ((pos - MEM_BASE) >> 9) & 0xff);
-  outb(CRT_ADDR_REG, CRT_CURSOR_L);
-  outb(CRT_DATA_REG, ((pos - MEM_BASE) >> 1) & 0xff);
+    outb(CRT_ADDR_REG, CRT_CURSOR_H); // 光标高地址
+    outb(CRT_DATA_REG, ((pos - MEM_BASE) >> 9) & 0xff);
+    outb(CRT_ADDR_REG, CRT_CURSOR_L);
+    outb(CRT_DATA_REG, ((pos - MEM_BASE) >> 1) & 0xff);
 }
 
 void console_clear()
 {
-  screen = MEM_BASE;
-  pos = MEM_BASE;
-  x = y = 0;
-  set_cursor();
-  set_screen();
+    screen = MEM_BASE;
+    pos = MEM_BASE;
+    x = y = 0;
+    set_cursor();
+    set_screen();
 
-  u16 *ptr = (u16 *)MEM_BASE;
-  while (ptr < MEM_END)
+    u16 *ptr = (u16 *)MEM_BASE;
+    while (ptr < MEM_END)
     {
-      *ptr++ = erase;
+        *ptr++ = erase;
     }
 }
 
 // 向上滚屏
 static void scroll_up()
 {
-  if (screen + SCR_SIZE + ROW_SIZE < MEM_END)
+    if (screen + SCR_SIZE + ROW_SIZE < MEM_END)
     {
-      u32 *ptr = (u32 *)(screen + SCR_SIZE);
-      for (size_t i = 0; i < WIDTH; i++)
+        u32 *ptr = (u32 *)(screen + SCR_SIZE);
+        for (size_t i = 0; i < WIDTH; i++)
         {
-          *ptr++ = erase;
+            *ptr++ = erase;
         }
-      screen += ROW_SIZE;
-      pos += ROW_SIZE;
+        screen += ROW_SIZE;
+        pos += ROW_SIZE;
     }
-  else
+    else
     {
-      memcpy(MEM_BASE, screen, SCR_SIZE);
-      pos -= (screen - MEM_BASE);
-      screen = MEM_BASE;
+        memcpy(MEM_BASE, screen, SCR_SIZE);
+        pos -= (screen - MEM_BASE);
+        screen = MEM_BASE;
     }
-  set_screen();
+    set_screen();
 }
 
 static void command_lf()
 {
-  if (y + 1 < HEIGHT)
+    if (y + 1 < HEIGHT)
     {
-      y++;
-      pos += ROW_SIZE;
-      return;
+        y++;
+        pos += ROW_SIZE;
+        return;
     }
-  scroll_up();
+    scroll_up();
 }
 
 static void command_cr()
 {
-  pos -= (x << 1);
-  x = 0;
+    pos -= (x << 1);
+    x = 0;
 }
 
 static void command_bs()
 {
-  if (x)
+    if (x)
     {
-      x--;
-      pos -= 2;
-      *(u16 *)pos = erase;
+        x--;
+        pos -= 2;
+        *(u16 *)pos = erase;
     }
 }
 
 static void command_del()
 {
-  *(u16 *)pos = erase;
+    *(u16 *)pos = erase;
 }
 
 void console_write(char *buf, u32 count)
- {
-     char ch;
-     char *ptr = (char *)pos;
-     while (count--)
-     {
-         ch = *buf++;
-         switch (ch)
-         {
-         case ASCII_NUL:
-             break;
-         case ASCII_BEL:
-             // todo \a
-             break;
-         case ASCII_BS:
-             command_bs();
-             break;
-         case ASCII_HT:
-             break;
-         case ASCII_LF:
-             command_lf();
-             command_cr();
-             break;
-         case ASCII_VT:
-             break;
-         case ASCII_FF:
-             command_lf();
-             break;
-         case ASCII_CR:
-             command_cr();
-             break;
-         case ASCII_DEL:
-             command_del();
-             break;
-         default:
-             if (x >= WIDTH)
-             {
-                 x -= WIDTH;
-                 pos -= ROW_SIZE;
-                 command_lf();
-             }
+{
+    char ch;
+    char *ptr = (char *)pos;
+    while (count--)
+    {
+        ch = *buf++;
+        switch (ch)
+        {
+        case ASCII_NUL:
+            break;
+        case ASCII_BEL:
+            // todo \a
+            break;
+        case ASCII_BS:
+            command_bs();
+            break;
+        case ASCII_HT:
+            break;
+        case ASCII_LF:
+            command_lf();
+            command_cr();
+            break;
+        case ASCII_VT:
+            break;
+        case ASCII_FF:
+            command_lf();
+            break;
+        case ASCII_CR:
+            command_cr();
+            break;
+        case ASCII_DEL:
+            command_del();
+            break;
+        default:
+            if (x >= WIDTH)
+            {
+                x -= WIDTH;
+                pos -= ROW_SIZE;
+                command_lf();
+            }
 
-             *ptr = ch;
-             ptr++;
-             *ptr = attr;
-             ptr++;
+            *ptr = ch;
+            ptr++;
+            *ptr = attr;
+            ptr++;
 
-             pos += 2;
-             x++;
-             break;
-         }
-     }
-     set_cursor();
- }
+            pos += 2;
+            x++;
+            break;
+        }
+    }
+    set_cursor();
+}
 
 void console_init()
 {
-  console_clear();
+    console_clear();
 }
-
