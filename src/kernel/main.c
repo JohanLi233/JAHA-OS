f6392eacd01532a0353d8e93e126e744ce9a76c5
JohanLi2333 <k20020910@kcl.ac.uk>
2022-02-19 11:44:10 +0000

    console

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,23 +1,12 @@
 #include <JAHA_OS/JAHA_OS.h>
 #include <JAHA_OS/types.h>
 #include <JAHA_OS/io.h>
-
-#define CRT_ADDR_REG 0x3d4
-#define CRT_DATA_REG 0x3d5
-#define CRT_CURSOR_H 0xe
-#define CRT_CURSOR_L 0xf
+#include <JAHA_OS/console.h>
 
 void kernel_init()
 {
-  outb(CRT_ADDR_REG, CRT_CURSOR_H);
-  u16 pos = inb(CRT_DATA_REG) << 8;
-  outb(CRT_ADDR_REG, CRT_CURSOR_L);
-  pos |= inb(CRT_DATA_REG);
+  console_init();
 
-  outb(CRT_ADDR_REG, CRT_CURSOR_H);
-  outb(CRT_DATA_REG, 0);
-  outb(CRT_ADDR_REG, CRT_CURSOR_L);
-  outb(CRT_DATA_REG, 0);
-
+  console_write("Hello", 5);
   return;
 }
